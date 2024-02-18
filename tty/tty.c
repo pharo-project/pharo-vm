10c9f57389a61effd718fceeceee34c26b09b3b4
Kris <1611248+Rinzwind@users.noreply.github.com>
2024-02-18 10:12:25 +0100

    Made ‘tty_spawn’ also check for errors when opening the slave pseudo-terminal device and setting up the standard file descriptors.

--- tty/tty.c
+++ tty/tty.c
@@ -10,7 +10,19 @@
 #include <stdio.h>
 #include <sys/ioctl.h>
 
-#define CHECK_ERROR(exp) if ((exp) == -1) { printf("Error in %s at %s: %s\n", __func__, #exp, strerror(errno)); exit(1); }
+#define CHECK_NULL(exp) \
+    if ((exp) == NULL) { \
+        exit(1); \
+    }
+#define CHECK_ERROR(exp) \
+    if ((exp) == -1) { \
+        exit(1); \
+    }
+#define CHECK_ERROR_PRINT(exp) \
+    if ((exp) == -1) { \
+        printf("Error in %s at %s: %s\n", __func__, #exp, strerror(errno)); \
+        exit(1); \
+    }
 
 pid_t tty_spawn(int fdm, const char *path, char *const argv[], char *const envp[])
     /*
@@ -22,10 +34,12 @@ pid_t tty_spawn(int fdm, const char *path, char *const argv[], char *const envp[
         device as returned by 'posix_openpt'. The next three arguments
         should be given as expected by 'execve'. The process ID of the
         new process is returned, or -1 if one could not be created. If
-        the process is created but fails to create the session, set
-        the controlling terminal or execute the file, it exits with
-        status 1 after printing an error message on the terminal that
-        indicates the call that failed.
+        the process is created but fails to open the slave device, or
+        set up the standard file descriptors, it exits with status 1.
+        If it fails to create the session, set the controlling
+        terminal or execute the file, it exits with status 1 after
+        printing an error message on the terminal that indicates the
+        call that failed.
         
         Provided through a library included with the VM to support
         Pharo terminal emulators: it can only be partially replicated
@@ -36,18 +50,21 @@ pid_t tty_spawn(int fdm, const char *path, char *const argv[], char *const envp[
     pid_t pid = fork();
     if (pid == 0)
     {
-        int fds = open(ptsname(fdm), O_RDWR);
-        close(fdm);
-        close(0);
-        close(1);
-        close(2);
-        dup(fds);
-        dup(fds);
-        dup(fds);
-        close(fds);
-        CHECK_ERROR(setsid());
-        CHECK_ERROR(ioctl(0, TIOCSCTTY, 0));
-        CHECK_ERROR(execve(path, argv, envp));
+        char *sname;
+        CHECK_NULL(sname = ptsname(fdm));
+        int fds;
+        CHECK_ERROR(fds = open(sname, O_RDWR));
+        CHECK_ERROR(close(fdm));
+        CHECK_ERROR(close(0));
+        CHECK_ERROR(close(1));
+        CHECK_ERROR(close(2));
+        CHECK_ERROR(dup(fds));
+        CHECK_ERROR(dup(fds));
+        CHECK_ERROR(dup(fds));
+        CHECK_ERROR(close(fds));
+        CHECK_ERROR_PRINT(setsid());
+        CHECK_ERROR_PRINT(ioctl(0, TIOCSCTTY, 0));
+        CHECK_ERROR_PRINT(execve(path, argv, envp));
     }
     return pid;
 }
