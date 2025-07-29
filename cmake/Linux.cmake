b2656049e7f9fd3a33e063124b7f2096acebfa5c
Maxim Cournoyer <maxim@guixotic.coop>
2025-07-29 10:29:27 +0900

    Linux.cmake: normalize indentation

--- cmake/Linux.cmake
+++ cmake/Linux.cmake
@@ -76,19 +76,17 @@ macro(configure_installables INSTALL_COMPONENT)
       DESTINATION "lib"
       USE_SOURCE_PERMISSIONS
       COMPONENT ${INSTALL_COMPONENT})
-	install(
-		DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/build/libffi/install/lib/"
-		DESTINATION "lib"
-		USE_SOURCE_PERMISSIONS
-		COMPONENT ${INSTALL_COMPONENT}
-		FILES_MATCHING PATTERN ${DYLIB_EXT})
-
-
-	install(
-	    DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/extracted/vm/include/unix/"
-	    DESTINATION include/pharovm
-	    COMPONENT include
-	    FILES_MATCHING PATTERN *.h)
+    install(
+      DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/build/libffi/install/lib/"
+      DESTINATION "lib"
+      USE_SOURCE_PERMISSIONS
+      COMPONENT ${INSTALL_COMPONENT}
+      FILES_MATCHING PATTERN ${DYLIB_EXT})
+    install(
+      DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/include/unix/"
+      DESTINATION include/pharovm
+      COMPONENT include
+      FILES_MATCHING PATTERN *.h)
 endmacro()
 
 macro(add_required_libs_per_platform)
