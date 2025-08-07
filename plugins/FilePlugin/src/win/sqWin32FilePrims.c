6ffb14d4eb64a440399c598736332a185db0a4d2
Pablo Tesone <tesonep@gmail.com>
2025-08-07 21:19:21 +0200

    Fixing warnings

--- plugins/FilePlugin/src/win/sqWin32FilePrims.c
+++ plugins/FilePlugin/src/win/sqWin32FilePrims.c
@@ -541,7 +541,7 @@ size_t sqFileWriteFromAt(SQFile *f, size_t count, char* byteArrayIndex, size_t s
 EXPORT(void) aioEnableExternalHandler(int fd, HANDLE handle, void *clientData, aioHandler handlerFn, int mask);
 
 EXPORT(void)
-handleWaitOnStream(int fd, void *clientData, int flag){
+handleWaitOnStream(sqInt fd, void *clientData, int flag){
 	interpreterProxy->signalSemaphoreWithIndex((sqInt)clientData);
 	aioDisable(fd);
 }
