3a9b6ee4e380e96361af748442d24716d268b66f
Pablo Tesone <tesonep@gmail.com>
2025-11-19 16:34:31 +0100

    Fix the signature of positive32BitValueOf to be unsigned int everywhere

--- include/pharovm/common/memoryAccess.h
+++ include/pharovm/common/memoryAccess.h
@@ -280,8 +280,13 @@ typedef unsigned long long usqIntptr_t;
 # define asIEEE32BitWord(val) (*((unsigned int*)&val))
 #endif /* USE_INLINE_MEMORY_ACCESSORS */
 
+#ifndef long32At
 #define long32At	intAt
+#endif
+
+#ifndef long32Atput  
 #define long32Atput	intAtput
+#endif
 
 /* platform-dependent float conversion macros.
  * Note: Second argument must be a variable name, not an expression!
