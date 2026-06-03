8cb684fac95cff2b9827bbb2655afe5accce260c
guillep <guillermopolito@gmail.com>
2026-06-03 14:31:01 +0200

    Add back openssl 1.0 library for ssl plugin

--- cmake/plugins.cmake
+++ cmake/plugins.cmake
@@ -68,6 +68,9 @@ if(FEATURE_PLUGIN_SSL)
     else()
         find_package(OpenSSL REQUIRED)
         target_link_libraries(SqueakSSL PRIVATE OpenSSL::SSL OpenSSL::Crypto)
+        # The VM builds on an ubuntu with openssl 1.0, thus the ssl plugin links to it.
+        # Ship ssl 1.0 with the VM, so the ssl plugin loads
+        add_third_party_dependency("openssl-1.0.2q")
     endif()
 endif()
 
